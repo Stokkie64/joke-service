1a5546116960b5d54dc6316a56ce90c0f5e7691c
Arjen Singels <asingels@afreximbank.com>
2022-07-09 14:25:12 +0200

    added todos

--- src/main/java/nl/ns/assignmentmicroservice/controllers/JokeController.java
+++ src/main/java/nl/ns/assignmentmicroservice/controllers/JokeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 public class JokeController {
+    //TODO: Add integration testing
 
     @Autowired
     JokeServiceImpl service;
--- src/main/java/nl/ns/assignmentmicroservice/services/JokeServiceImpl.java
+++ src/main/java/nl/ns/assignmentmicroservice/services/JokeServiceImpl.java
@@ -12,12 +12,16 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 @Service
 public class JokeServiceImpl implements JokeService {
+    //TODO: add unit testing
 
     @Autowired
     RestTemplate restTemplate;
 
     @Override
     public Joke getRandomJoke() {
+        //TODO: Externalize api configs
+        //TODO: Filter for shortest joke (query param)
+        //TODO: Filter safe jokes (query param) not sexist or explicit.
         JokeapiResultDTO result = restTemplate.getForObject("https://v2.jokeapi.dev/joke/Any?type=single&amount=16", JokeapiResultDTO.class);
         log.debug("Result from joke api : " + result);
         if (result!= null && !result.getJokes().isEmpty()) {
